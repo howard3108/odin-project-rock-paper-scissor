b6a7659b606cac2ff5696685a697d249fc7b4b3c
Howard Chen <howard3108@github.com>
2023-07-15 22:24:21 -0400

    feat: added logic to reset game when hit 5

--- index.js
+++ index.js
@@ -8,12 +8,21 @@ window.onload = () => {
     // add event listener to click and playRound
     rock.addEventListener("click", e => {
         playRound('rock', getComputerChoice())
+
+        // reset game if comp or user win 5 times
+        resetGame()
     })
     paper.addEventListener("click", e => {
-        playRound('rock', getComputerChoice())
+        playRound('paper', getComputerChoice())
+
+        // reset game if comp or user win 5 times
+        resetGame()
     })
     scissor.addEventListener("click", e => {
-        playRound('rock', getComputerChoice())
+        playRound('scissor', getComputerChoice())
+
+        // reset game if comp or user win 5 times
+        resetGame()
     })
 }
 
@@ -44,5 +53,25 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissor' || playerSelection === 'scissor' && computerSelection === 'rock'){
         // select the loseScore's innerText convert it to int and add 1
         loseScore.innerText = +loseScore.innerText + 1
+    }
+}
+
+// reset game when user or comp hit 5 games won
+function resetGame() {
+    // constants
+    const winScore = document.querySelector('#win-score');
+    const loseScore = document.querySelector('#lose-score');
+    let winScoreText = parseInt(document.querySelector('#win-score').innerText);
+    let loseScoreText = parseInt(document.querySelector('#lose-score').innerText);
+
+    // if comp or user win 5, alert user and reset score
+    if (winScoreText >= 5) {
+        alert('You win!');
+        winScore.innerText = '0';
+        loseScore.innerText = '0';
+    } else if (loseScoreText >= 5) {
+        alert('You lose!');
+        winScore.innerText = '0';
+        loseScore.innerText = '0';
     }
 }
