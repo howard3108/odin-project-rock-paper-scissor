c5f5b6b47a59dfdc4f0ec2c5b61c8c794e006c22
Howard Chen <howard3108@github.com>
2023-07-25 23:07:42 -0400

    feat: added logic for displaying result modal

--- index.js
+++ index.js
@@ -11,37 +11,41 @@ window.onload = () => {
   // add event listener to click and playRound
   rock.addEventListener('click', e => {
     playRound('rock', getComputerChoice())
-
     // reset game if comp or user win 5 times
-    resetGame()
+    checkScore()
   })
   paper.addEventListener('click', e => {
     playRound('paper', getComputerChoice())
 
     // reset game if comp or user win 5 times
-    resetGame()
+    checkScore()
   })
   scissor.addEventListener('click', e => {
     playRound('scissor', getComputerChoice())
 
     // reset game if comp or user win 5 times
-    resetGame()
+    checkScore()
   })
 
   rules.addEventListener('click', () => {
-    modal.style.display = "block"
+    modal.style.display = 'block'
   })
 
   close.addEventListener('click', () => {
-    modal.style.display = "none"
+    modal.style.display = 'none'
   })
 
   window.addEventListener('click', (event) => {
     if (event.target === modal) {
-      modal.style.display = "none"
+      modal.style.display = 'none'
     }
   })
 }
+
+// constant
+const winScore = document.querySelector('#winScore')
+const loseScore = document.querySelector('#loseScore')
+
 
 // Get computer's choice of rock paper or scissor
 function getComputerChoice() {
@@ -59,9 +63,7 @@ function getComputerChoice() {
 // Play a round of game with user then return the result
 function playRound(playerSelection, computerSelection) {
 
-  // constant
-  const winScore = document.querySelector('#win-score')
-  const loseScore = document.querySelector('#lose-score')
+  const roundMessage = document.querySelector('#roundMessage')
 
   // add logic to increase score when user loses or wins
   if (playerSelection === 'rock' && computerSelection === 'scissor' ||
@@ -71,6 +73,9 @@ function playRound(playerSelection, computerSelection) {
     // select the winScore's innerText convert it to int and add 1
     winScore.innerText = +winScore.innerText + 1
 
+    // change innerText and display shows
+    roundMessage.innerText = 'You win this round!'
+    roundMessage.style.display = 'block'
   } else if (playerSelection === 'rock' && computerSelection === 'paper' ||
     playerSelection === 'paper' && computerSelection === 'scissor' ||
     playerSelection === 'scissor' && computerSelection === 'rock') {
@@ -78,28 +83,70 @@ function playRound(playerSelection, computerSelection) {
     // select the loseScore's innerText convert it to int and add 1
     loseScore.innerText = +loseScore.innerText + 1
 
+    // change innerText and display shows
+    roundMessage.innerText = 'You lose this round!'
+    roundMessage.style.display = 'block'
   } else {
-    console.log('Tied')
+
+    // change innerText and display shows
+    roundMessage.innerText = 'Tied!'
+    roundMessage.style.display = 'block'
   }
 }
 
-// reset game when user or comp hit 5 games won
-function resetGame() {
+// check score function
+function checkScore() {
 
   // constants
-  const winScore = document.querySelector('#win-score')
-  const loseScore = document.querySelector('#lose-score')
-  let winScoreText = parseInt(document.querySelector('#win-score').innerText)
-  let loseScoreText = parseInt(document.querySelector('#lose-score').innerText)
+  const playAgain = document.querySelector('#playAgain')
+  const resultModal = document.querySelector('#resultScreen')
+  let winScoreText = parseInt(document.querySelector('#winScore').innerText)
+  let loseScoreText = parseInt(document.querySelector('#loseScore').innerText)
 
-  // if comp or user win 5, alert user and reset score
+  // check score if score is 5 open modal
   if (winScoreText >= 5) {
-    alert(`You win! ${winScoreText} to ${loseScoreText}`)
-    winScore.innerText = '0'
-    loseScore.innerText = '0'
+    showCorrectMessage()
+    resultModal.style.display = 'block'
+    playAgain.addEventListener('click', resetGame)
   } else if (loseScoreText >= 5) {
-    alert(`You lose! ${loseScoreText} to ${winScoreText}`)
-    winScore.innerText = '0'
-    loseScore.innerText = '0'
+    showCorrectMessage()
+    resultModal.style.display = 'block'
+    playAgain.addEventListener('click', resetGame)
   }
+}
+
+function showCorrectMessage() {
+
+  // constants
+  const resultModalH2 = document.querySelector('#resultModalH2')
+  const yourScore = document.querySelector('#yourScore')
+  const compScore = document.querySelector('#compScore')
+  const winScoreText = document.querySelector('#winScore').innerHTML
+  const loseScoreText = document.querySelector('#loseScore').innerHTML
+
+  // Set the results modal to have correct message and displays score
+  if (winScoreText > loseScoreText) {
+    resultModalH2.innerText = 'You Win!'
+    yourScore.innerHTML = `${winScoreText}`
+    compScore.innerHTML = `${loseScoreText}`
+  }
+  else {
+    resultModalH2.innerText = 'You Lost.'
+    yourScore.innerHTML = `${winScoreText}`
+    compScore.innerHTML = `${loseScoreText}`
+  }
+}
+
+
+// reset game when user or comp hit 5 games won
+function resetGame() {
+  // constants
+  const resultModal = document.querySelector('#resultScreen')
+  const roundMessage = document.querySelector('#roundMessage')
+
+  // hide the roundMessage and resultModal and reset the score
+  roundMessage.style.display = 'none'
+  resultModal.style.display = 'none'
+  winScore.innerText = '0'
+  loseScore.innerText = '0'
 }
